dbcce91a94d909e1bb0d644d7a6c3a103a87e865
Nastin2022 <nastinmfena@gmail.com>
2022-10-30 19:33:42 +0800

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Nastin.
+            <br className="hidden lg:inline-block" />I love to build amazing
+            stuffs.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+          Over 4+ years of experience as a web developer specializing specifically in
+WordPress and Shopify development. Highly experienced in WordPress plugins
+such as Elementor Pro, Woo commerce and several of its extensions, Learn Press,
+API Integration, Theme and Plugin customization, etc.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Past Works
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "WordPress",
+    subtitle: "Petruth IT Solutions",
+    description:
+      "We help ideas and businesses unleash their full potentials through modern, responsive, and dynamic website designs that make them stand out.",
+    image: "./petruth.png",
+    link: "https://petruthit.com/",
+  },
+  {
+    title: "Shopify",
+    subtitle: "The Chaless HongKong",
+    description:
+      "This is a Shopify site built for The Chaless. The Chaless is beauty and wellness company in Hong Kong.",
+    image: "./thechaless.png",
+    link: "https://thechaless.com/",
+  },
+  {
+    title: "WordPress",
+    subtitle: "Living Lotus Store",
+    description:
+      "This is an E-commerce site built purely with Elementor with Wholesale and retail features.",
+    image: "./livinglotus.png",
+    link: "https://livinglotus.net/",
+  },
+  {
+    title: "React JS",
+    subtitle: "Fitness by Sarah Taylor",
+    description:
+      "Sarah is a fitness instructor based in Toronto Canada. This website is linked to her mobile app where she offers different kinds of fitness journey plans.",
+    image: "./fitness.png",
+    link: "http://fitnessbysarahtaylor.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "WordPress",
+  "React",
+  "Shopify",
+  "PHP",
+  "HTML/CSS",
+  "Material UI",
+];
